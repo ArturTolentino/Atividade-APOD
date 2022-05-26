9304f15544e39c8d80ba91fa9c134b39ba0f8773
ArturTolentino <97901283+ArturTolentino@users.noreply.github.com>
2022-05-26 01:48:47 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let req = new XMLHttpRequest();
+
+req.open("GET", "https://api.nasa.gov/planetary/apod?api_key=pOwBAdf2wrjTLhAdv7bD6SvZEPyaupu1osGM41nW")
+
+reqDate.addEventListener("load", function(event) {
+  if(reqDate.status == 200) {
+
+
+    
+  }
