dedcc70308649cbb93f03fc21be4c3de2367cb4f
ArturTolentino <97901283+ArturTolentino@users.noreply.github.com>
2022-05-27 16:16:50 -0300

    Funcional novamente.
    Fiz algo que fez o commit passado ser revertido.

--- javascript.js
+++ javascript.js
@@ -10,10 +10,11 @@ function chama() {
     fetch('https://api.nasa.gov/planetary/apod?api_key=pOwBAdf2wrjTLhAdv7bD6SvZEPyaupu1osGM41nW' + "&" + "date=" + btn ).then(r => {r.json().then(resposta => {
       console.log(resposta)
       
-      document.getElementById('frame').contentWindow.document.style. = resposta.url
-
-  
-  
+      // document.getElementById('frame').contentWindow.document.style. = resposta.url
+      corpo.style.backgroundImage = resposta.url
+      document.getElementById("title").innerText = resposta.title
+      document.getElementById("date").innerText = resposta.date
+      document.getElementById("imagem").src = resposta.url
     })
     
   })
